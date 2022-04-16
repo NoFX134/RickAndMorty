aff5e5b990955dcbc8319df202e8b03abe2497bb
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-16 17:26:12 +0700

    RecycleView add success

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -3,31 +3,43 @@ package com.example.rickandmorty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.Repository.Repository
+import com.example.rickandmorty.adapter.CharacterAdapter
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewModel: MainViewModel
+    private val characterAdapter by lazy { CharacterAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecycleview()
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
         viewModel.getCharacter(1)
-        viewModel.myResponse.observe(this) { response ->
+        viewModel.myResponse.observe(this, Observer { response ->
             if (response.isSuccessful) {
-                val resultList = response.body()?.results
-                resultList?.forEach {
-                    Log.d("Response", it?.name.toString())
-                    Log.d("Response", it?.gender.toString())
-                    Log.d("Response", it?.image.toString())
-                    Log.d("Response", "xxxxxxxxxxxxxxxxxxx ")
-                }
+                characterAdapter.seData(response.body()?.results)
             } else {
-                Log.d("Response", response.code().toString())
+                Toast.makeText(
+                    this, "Ошибка ${response.code().toString()}",
+                    Toast.LENGTH_LONG
+                ).show()
             }
-        }
+        })
+    }
+
+    private fun setupRecycleview() {
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = characterAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
