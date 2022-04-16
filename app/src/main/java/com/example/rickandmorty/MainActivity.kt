1b451c72fc84ced598212cc392929f991e404cb7
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-16 18:01:38 +0700

    xml Correct

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rickandmorty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,16 +16,34 @@ import com.example.rickandmorty.adapter.CharacterAdapter
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var button: Button
     private val characterAdapter by lazy { CharacterAdapter() }
-
+    private var userId = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
         setupRecycleview()
+
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getCharacter(1)
+        request(userId)
+        button.setOnClickListener {
+            userId += 1
+            request(userId)
+        }
+    }
+
+
+    private fun setupRecycleview() {
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = characterAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun request(userId: Int) {
+        viewModel.getCharacter(userId)
         viewModel.myResponse.observe(this, Observer { response ->
             if (response.isSuccessful) {
                 characterAdapter.seData(response.body()?.results)
@@ -34,12 +53,7 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_LONG
                 ).show()
             }
-        })
-    }
 
-    private fun setupRecycleview() {
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.adapter = characterAdapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        })
     }
 }
--- app/src/main/java/com/example/rickandmorty/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/adapter/CharacterAdapter.kt
@@ -3,18 +3,21 @@ package com.example.rickandmorty.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.Model.Character
 import com.example.rickandmorty.Model.ResultsItem
 import com.example.rickandmorty.R
+import com.squareup.picasso.Picasso
 
 
 class CharacterAdapter() : RecyclerView.Adapter<CharacterAdapter.MyViewHolder>() {
-    private var Character = Character()
-    private var CharacterList = Character.results
+    private var character = Character()
+    private var characterList = character.results
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.image)
         val name: TextView = itemView.findViewById(R.id.name)
         val gender: TextView = itemView.findViewById(R.id.gender)
         val species: TextView = itemView.findViewById(R.id.species)
@@ -27,17 +30,18 @@ class CharacterAdapter() : RecyclerView.Adapter<CharacterAdapter.MyViewHolder>()
     }
 
     override fun getItemCount(): Int {
-        return CharacterList?.size ?: -1
+        return characterList?.size ?: -1
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.name.text = CharacterList?.get(position)?.name.toString()
-        holder.gender.text = CharacterList?.get(position)?.gender.toString()
-        holder.species.text = CharacterList?.get(position)?.species.toString()
+        Picasso.get().load(characterList?.get(position)?.image).into(holder.image)
+        holder.name.text = characterList?.get(position)?.name.toString()
+        holder.gender.text = characterList?.get(position)?.gender.toString()
+        holder.species.text = characterList?.get(position)?.species.toString()
     }
 
     fun seData(newList: List<ResultsItem?>?) {
-        CharacterList = newList
+        characterList = newList
         notifyDataSetChanged()
 
     }
