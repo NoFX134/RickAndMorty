22d6d5af7bb46c2b19b3651395f212068f5b434a
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-04-17 12:40:22 +0700

    Correct second Activity

--- app/src/main/java/com/example/rickandmorty/API/CharacterApi.kt
+++ app/src/main/java/com/example/rickandmorty/API/CharacterApi.kt
@@ -4,10 +4,14 @@ import com.example.rickandmorty.Model.Character
 import com.example.rickandmorty.Model.ResultsItem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApi {
     @GET("character")
 
     suspend fun getCharacter(@Query("page") page:Int): Response<Character>
+
+    @GET("character/{characterNumber}")
+    suspend fun getDetailCharacter (@Path("characterNumber") characterNumber: Int): Response<ResultsItem>
 }
--- app/src/main/java/com/example/rickandmorty/Repository/Repository.kt
+++ app/src/main/java/com/example/rickandmorty/Repository/Repository.kt
@@ -4,9 +4,13 @@ import com.example.rickandmorty.API.RetrofitInstance
 import com.example.rickandmorty.Model.Character
 import com.example.rickandmorty.Model.ResultsItem
 import retrofit2.Response
+import retrofit2.http.Path
 
 class Repository {
     suspend fun getCharacter(userId: Int): Response<Character> {
         return RetrofitInstance.api.getCharacter(userId)
+    }
+    suspend fun getDetailCharacter ( characterNumber: Int): Response<ResultsItem>{
+        return RetrofitInstance.api.getDetailCharacter (characterNumber)
     }
 }
